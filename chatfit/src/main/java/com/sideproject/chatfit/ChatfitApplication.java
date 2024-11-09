a11aa09f224d9b4c532bdfd4a40046d31246b610
YOONSIWON <ytldnjs@naver.com>
2024-11-09 20:22:01 +0900

    환경설정 - 윤시원

---
+++ chatfit/src/main/java/com/sideproject/chatfit/ChatfitApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.chatfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatfitApplication.class, args);
+	}
+
+}
---
+++ chatfit/src/test/java/com/sideproject/chatfit/ChatfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.chatfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
